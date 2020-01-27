afbe6a3c85a10889e617fcc4dbcd182ef9eed8fe
Jakub <jflaska@baosystems.com>
2020-01-27 11:37:20 -0500

    minimalistic header bar

--- src/index.js
+++ src/index.js
@@ -1,4 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'));
+
+import {HeaderBar} from '@dhis2/ui-widgets';
+import {Provider} from '@dhis2/app-runtime';
+import {init, config} from 'd2';
+
+const baseUrl = 'https://newrelease.datim.org/';
+
+config.baseUrl = baseUrl + 'api';
+config.i18n.sources.add('i18n.txt');
+
+function HeaderbarPackage(){
+    return <Provider config={{baseUrl: baseUrl, apiVersion: '30'}}>
+        <HeaderBar/>
+    </Provider>
+}
+
+init().then(()=>{
+    ReactDOM.render(<HeaderbarPackage/>, document.getElementById('header-bar'));
+});
+
+
